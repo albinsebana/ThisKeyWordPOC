1fed15740ce27f05c6fd10e60d3ec80da3907ad7
Albin Kurian <Albin.Kurian@asl.mum.sst>
2023-08-11 22:06:05 +0530

    This keyword Example poc

---
+++ KeyWordThis/Model/ThisKey.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyWordThis.Model
+{
+    internal class ThisKey
+    {
+        private int number;
+        private int value;
+        private int myValue;
+
+
+        public void Example1(int myValue)
+        {
+            this.myValue = myValue; // Use 'this' to refer to the instance variable
+        }
+
+
+        public void Example2(int number)
+        {
+            this.number = number;
+        }
+
+        public void PrintNumber()
+        {
+            Console.WriteLine($"The number is: {this.number}"); //used to access the  variable within the instance method.
+        }
+    }
+}
+//it is also used in  constructor chaining.
+//public MyClass() : this(0)
---
+++ KeyWordThis/Program.cs
@@ -0,0 +1,24 @@
+﻿using KeyWordThis.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyWordThis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ThisKey myObject = new ThisKey();
+
+            myObject.Example1(42);
+            myObject.Example2(123);
+            myObject.PrintNumber();
+
+            Console.ReadLine();
+
+        }
+    }
+}
